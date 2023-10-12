447ad9d354ada061a517c9b8481142ea8bb21035
alancccc8 <93360333+alancccc8@users.noreply.github.com>
2023-10-12 14:44:33 +0100

    Add files via upload

---
+++ volcano_beta-master/vue.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://zehuan-wang.edinburgh.domains/fatfree/Volcano-Back/', // 接口的前缀
+        ws: true, // 代理 websocked
+        changeOrigin: true, // 虚拟的站点需要更管origin
+        pathRewrite: {
+          '/api': '', // 重写路径
+        },
+      },
+    },
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Volcano-Front/beta/'
+    : '/',
+  outputDir: 'beta',
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "@/styles/mixin.scss";`,
+      },
+    },
+  },
+};
